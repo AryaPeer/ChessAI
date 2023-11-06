d1a0df0a32c0ae1ed17783225928bfcadb0493a1
Monk-Mental <85086354+AryaPeer@users.noreply.github.com>
2023-11-05 22:29:22 -0500

    Basic UI Functionality

--- script.js
+++ script.js
@@ -3,27 +3,57 @@
 
 var board = null
 var game = new Chess()
+var $status = $('#status')
+var $fen = $('#fen')
+var $pgn = $('#pgn')
+var whiteSquareGrey = '#a9a9a9'
+var blackSquareGrey = '#696969'
 
-function onDragStart (source, piece, position, orientation) {
+function removeGreySquares() {
+  $('#myBoard .square-55d63').css('background', '')
+}
+
+function greySquare(square) {
+  var $square = $('#myBoard .square-' + square)
+
+  var background = whiteSquareGrey
+  if ($square.hasClass('black-3c85d')) {
+    background = blackSquareGrey
+  }
+
+  $square.css('background', background)
+}
+
+
+function onDragStart(source, piece, position, orientation) {
   // do not pick up pieces if the game is over
   if (game.game_over()) return false
 
   // only pick up pieces for White
-  if (piece.search(/^b/) !== -1) return false
+  if (game.turn() === 'w' && piece.search(/^b/) !== -1) {
+    return false
+  }
 }
 
-function makeRandomMove () {
-  var possibleMoves = game.moves()
+function makeRandomMove() {
+  // Check if it's the black player's turn
+  if (game.turn() === 'b') {
+    var possibleMoves = game.moves();
 
-  // game over
-  if (possibleMoves.length === 0) return
+    // Game over
+    if (possibleMoves.length === 0) return;
 
-  var randomIdx = Math.floor(Math.random() * possibleMoves.length)
-  game.move(possibleMoves[randomIdx])
-  board.position(game.fen())
+    var randomIdx = Math.floor(Math.random() * possibleMoves.length);
+    game.move(possibleMoves[randomIdx]);
+    board.position(game.fen());
+
+    // After making a move for the black player, update the status
+    updateStatus();
+  }
 }
 
-function onDrop (source, target) {
+function onDrop(source, target) {
+  removeGreySquares()
   // see if the move is legal
   var move = game.move({
     from: source,
@@ -35,13 +65,70 @@ function onDrop (source, target) {
   if (move === null) return 'snapback'
 
   // make random legal move for black
+  updateStatus()
   window.setTimeout(makeRandomMove, 250)
+}
+
+function onMouseoverSquare(square, piece) {
+  // get list of possible moves for this square
+  var moves = game.moves({
+    square: square,
+    verbose: true
+  })
+
+  // exit if there are no moves available for this square
+  if (moves.length === 0 || piece.search(/^b/) !== -1) return
+
+  // highlight the square they moused over
+  greySquare(square)
+
+  // highlight the possible squares for this piece
+  for (var i = 0; i < moves.length; i++) {
+    greySquare(moves[i].to)
+  }
+}
+
+function onMouseoutSquare(square, piece) {
+  removeGreySquares()
 }
 
 // update the board position after the piece snap
 // for castling, en passant, pawn promotion
-function onSnapEnd () {
+function onSnapEnd() {
   board.position(game.fen())
+}
+
+function updateStatus() {
+  var status = ''
+
+  var moveColor = 'White'
+  if (game.turn() === 'b') {
+    moveColor = 'Black'
+  }
+
+  // checkmate?
+  if (game.in_checkmate()) {
+    status = 'Game over, ' + moveColor + ' is in checkmate.'
+  }
+
+  // draw?
+  else if (game.in_draw()) {
+    status = 'Game over, drawn position'
+  }
+
+  // game still on
+  else {
+    status = moveColor + ' to move'
+
+    // check?
+    if (game.in_check()) {
+      status += ', ' + moveColor + ' is in check'
+    }
+  }
+
+  $status.html(status)
+  $fen.html(game.fen())
+  $pgn.html(game.pgn())
 }
 
 var config = {
@@ -49,6 +136,11 @@ var config = {
   position: 'start',
   onDragStart: onDragStart,
   onDrop: onDrop,
+  onMouseoutSquare: onMouseoutSquare,
+  onMouseoverSquare: onMouseoverSquare,
   onSnapEnd: onSnapEnd
 }
+
 board = Chessboard('myBoard', config)
+
+updateStatus()
