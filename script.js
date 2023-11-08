9fcba18a8cb0dcec79268bede6bcdfdc2459019d
Monk-Mental <85086354+AryaPeer@users.noreply.github.com>
2023-11-07 19:13:27 -0500

    Fix weird dragging glitches

--- script.js
+++ script.js
@@ -36,9 +36,11 @@ function onDragStart(source, piece, position, orientation) {
   // do not pick up pieces if the game is over
   if (game.isGameOver()) return false
 
-  // only pick up pieces for White
-  if (game.turn() === 'w' && piece.search(/^b/) !== -1) {
-    return false
+  if (
+    (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+    (game.turn() === 'b' && piece.search(/^w/) !== -1)
+  ) {
+    return false;
   }
 }
 
